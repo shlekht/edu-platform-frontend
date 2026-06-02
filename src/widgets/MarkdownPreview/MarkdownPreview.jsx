99b8b500d4d7906c587765d4610371ab2f55dbba
Nikita <322liquid@gmail.com>
2026-06-02 18:42:22 +0500

    markdown preview style ui change

--- src/widgets/MarkdownPreview/MarkdownPreview.jsx
+++ src/widgets/MarkdownPreview/MarkdownPreview.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactMarkdown from "react-markdown";
-import styles from "./MarkdownPreview.module.css";
+import "github-markdown-css/github-markdown-light.css";
+
 
 export const MarkdownPreview = ({ text }) => {
   return (
-    <div className={styles.container}>
+    <>
+    
+    <div className="markdown-body" style={{ backgroundColor: "#F3F4F6", padding: "5px", border: "1px solid #30363d" }}>
       <ReactMarkdown>{text}</ReactMarkdown>
     </div>
+    </>
   );
 };
 
